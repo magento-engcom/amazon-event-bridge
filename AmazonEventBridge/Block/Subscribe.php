bbf7739bb84a4f6efc43df7d54de208f096f76c3
meandor-ua <alex.semchyshyn@gmail.com>
2019-10-21 15:38:24 +0200

    MLEU bugfix: get a rid of object manager from the code

--- AmazonEventBridge/Block/Subscribe.php
+++ AmazonEventBridge/Block/Subscribe.php
@@ -7,26 +7,59 @@ declare(strict_types=1);
 
 namespace Magento\AmazonEventBridge\Block;
 
-use Magento\Framework\App\ObjectManager;
 use Magento\Framework\Data\Helper\PostHelper;
-use Magento\Framework\Escaper;
-use Magento\Store\Model\StoreManagerInterface;
+use Magento\Catalog\Api\ProductRepositoryInterface;
 
 class Subscribe extends \Magento\Catalog\Block\Product\View
 {
+    /**
+     * @var PostHelper
+     */
+    private $postDataHelper;
 
+    public function __construct(
+        PostHelper $postDataHelper,
+        \Magento\Catalog\Block\Product\Context $context,
+        \Magento\Framework\Url\EncoderInterface $urlEncoder,
+        \Magento\Framework\Json\EncoderInterface $jsonEncoder,
+        \Magento\Framework\Stdlib\StringUtils $string,
+        \Magento\Catalog\Helper\Product $productHelper,
+        \Magento\Catalog\Model\ProductTypes\ConfigInterface $productTypeConfig,
+        \Magento\Framework\Locale\FormatInterface $localeFormat,
+        \Magento\Customer\Model\Session $customerSession,
+        ProductRepositoryInterface $productRepository,
+        \Magento\Framework\Pricing\PriceCurrencyInterface $priceCurrency,
+        array $data = []
+    ) {
+        parent::__construct(
+            $context,
+            $urlEncoder,
+            $jsonEncoder,
+            $string,
+            $productHelper,
+            $productTypeConfig,
+            $localeFormat,
+            $customerSession,
+            $productRepository,
+            $priceCurrency,
+            $data
+        );
+
+        $this->postDataHelper = $postDataHelper;
+    }
+
+    /**
+     * @return string
+     */
     public function getPostParams()
     {
-        $storeManager = ObjectManager::getInstance()->get(StoreManagerInterface::class);
-        $escaper = ObjectManager::getInstance()->get(Escaper::class);
-        $postHelper = ObjectManager::getInstance()->get(PostHelper::class);
         $item = $this->getProduct();
 
-        $url = $storeManager->getStore()->getUrl('watchproduct/product/eventcontroller');
+        $url = $this->getUrl('watchproduct/product/eventcontroller');
         $params['product_sku'] = $item->getSku();
 
-        return $postHelper->getPostData(
-            $escaper->escapeUrl($url),
+        return $this->postDataHelper->getPostData(
+            $this->escapeUrl($url),
             $params
         );
     }
